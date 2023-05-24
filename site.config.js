9b6b112451997b6ef183f0240056bc889d63f0c3
dalu-me <76034667+dalu-me@users.noreply.github.com>
2023-05-24 23:15:47 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: "https://w.nexis.cc/w-icon.png",
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: '联系我们',
+      pageId: '9c55e75bb2a240db9880ef82649775db'
+    },
+    {
+      title: '关于',
+      pageId: '9c55e75bb2a240db9880ef82649775db'
+    }
+  ]
 })
