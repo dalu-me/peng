4f217200ac6bb4c3aaaa573edada4916cfc12ab3
dalu-me <76034667+dalu-me@users.noreply.github.com>
2023-05-25 14:13:02 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: "https://w.nexis.cc/w-icon.png",
+  defaultPageIcon: "https://w.nexis.cc/w-fav.ico",
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
